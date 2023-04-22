9b4a7f7199a24102ed8978d44c74a60775fbbbe7
Ivan Migalev <ivan.migalev@jetbrains.com>
2023-04-22 19:03:41 +0200

    CI: tune Ant logging in Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
+buildDir = file("gradle-build")
+
+ant.lifecycleLogLevel = AntBuilder.AntMessagePriority.INFO
+ant.properties["dir.machine.build-runtime"] = file(".build/eclipse/eclipse").path
+ant.importBuild("build/build.xml")
+
 tasks.wrapper {
     gradleVersion = "8.1.1"
     distributionType = Wrapper.DistributionType.ALL
 }
-
-buildDir = file("gradle-build")
-
-ant.properties["dir.machine.build-runtime"] = file(".build/eclipse/eclipse").path
-ant.importBuild("build/build.xml")
